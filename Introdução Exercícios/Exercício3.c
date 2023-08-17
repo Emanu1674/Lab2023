1cd681a04a3cd5a4b6422fc438cd294d7170e656
Emanuel Rigão <60308011+Emanu1674@users.noreply.github.com>
2023-08-17 15:17:20 -0300

    Update and rename Exercício3.c to Introdução Exercícios/Exercício3.c

--- Introdução Exercícios/Exercício3.c
+++ Introdução Exercícios/Exercício3.c
@@ -7,6 +7,6 @@ int main() {
     printf("Digite um valor inteiro, e um valor real: ");
     scanf("%d %f", &x, &y);
 
-    printf("Vari·vel x = %d\nVari·vel y = %.2f", x, y);
+    printf("Vari√°vel x = %d\nVari√°vel y = %.2f", x, y);
     return 0;
 }
