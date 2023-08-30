91d52cec780686f811418c1faddfbcf202991f00
Emanuel Rigão <60308011+Emanu1674@users.noreply.github.com>
2023-08-30 00:49:23 -0300

    Add files via upload

--- Seleção If Exercícios/Exercício5.c
+++ Seleção If Exercícios/Exercício5.c
@@ -5,10 +5,9 @@ int main(){
     double a, b, c, discriminante, raiz1, raiz2, parteReal, parteImagin;
     printf("\nInforme os coeficientes a, b e c: ");
     scanf("%lf%lf%lf",&a,&b,&c);
-
     discriminante = b * b - 4 * a * c;
 
-    //Discriminante é real maior que 0
+    //Discriminante real e raízes diferentes
 
     if(discriminante > 0){
         raiz1 = (-b + sqrt(discriminante)) / (2 * a);
@@ -16,13 +15,20 @@ int main(){
         printf("Raiz 1 = %.2lf e Raiz 2 = %.2lf", raiz1, raiz2);
     }
 
-    //Discriminante é igual a 0
+    //Discriminante real e raízes iguais
 
     else if(discriminante == 0){
-
+        raiz1 = raiz2 = -b / (2 * a);
+        printf("Raiz 1 = Raiz 2 = %.2lf;", raiz1);
     }
 
-    else
+    //Raízes não reais
+
+    else {
+        parteReal = -b / (2 * a);
+        parteImagin = sqrt(-discriminante) / (2 * a);
+        printf("Raiz 1 = %.2lf+%.2lfi e Raiz 2 = %.2f-%.2fi", parteReal, parteImagin, parteReal, parteImagin);
+    }
 
     return 0;
 }
